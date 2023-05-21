a71e02e1bd85c75d935ce8a9a1f017e80545122f
Suzu <sucrablue@gmail.com>
2023-05-21 11:31:25 +0900

    third commit

--- WordQuiz.py
+++ WordQuiz.py
@@ -4,17 +4,33 @@ import pandas as pd
 import random
 import os
 
-def load_flashcards(file_name):
-    df = pd.read_excel(file_name)
-    flashcards = df.to_dict('records')
-    return flashcards
+# These are the required columns in the Excel file
+REQUIRED_COLUMNS = ['Question', 'Answer']
+# This determines the number of multiple-choice options presented to the user for each question
+NUM_CHOICES = 4
 
+# Load the flashcards from an Excel file
+def load_flashcards(file_name):
+    try:
+        df = pd.read_excel(file_name)
+        # Check if the file has all the required columns
+        if not all(column in df.columns for column in REQUIRED_COLUMNS):
+            print("The file must have 'Question' and 'Answer' columns.")
+            return None
+        flashcards = df[REQUIRED_COLUMNS].to_dict('records')
+        return flashcards
+    except Exception as e:
+        print(f"Error loading file: {e}")
+        return None
+
+# Given a list of flashcards and a correct answer, this function generates multiple choices (1 correct and 3 incorrect)
 def get_multiple_choices(flashcards, correct_answer):
     incorrect_flashcards = [fc['Answer'] for fc in flashcards if fc['Answer'] != correct_answer]
-    choices = [correct_answer] + random.sample(incorrect_flashcards, 3)
-    choices.sort()
+    choices = [correct_answer] + random.sample(incorrect_flashcards, NUM_CHOICES - 1)
+    random.shuffle(choices)
     return choices
 
+# Display a question and its choices
 def display_question_and_choices(idx, num_questions, question, choices):
     print(f"Question {idx}/{num_questions}: {question}")
     for i, choice in enumerate(choices, start=1):
@@ -25,6 +41,9 @@ def quiz_flashcards(flashcards):
     num_questions = len(flashcards)
     random.shuffle(flashcards)
     incorrect_answers = []
+    
+    # Add a new variable to keep track of the current question number
+    current_question = 0
 
     for idx, flashcard in enumerate(flashcards, start=1):
         question = flashcard['Question']
@@ -36,6 +55,9 @@ def quiz_flashcards(flashcards):
 
         if user_answer.strip().lower() == 'stop':
             break
+
+        # Increment the current question number after a 'stop' has not been received
+        current_question += 1
 
         try:
             user_choice = choices[int(user_answer) - 1]
@@ -51,12 +73,42 @@ def quiz_flashcards(flashcards):
             incorrect_answers.append((question, user_choice, answer))
         print()
 
-    print(f"Your score: {num_correct}/{idx - 1}")
+    # Use current_question instead of idx - 1
+    print(f"Your score: {num_correct}/{current_question}")
 
     if incorrect_answers:
         print("\nReview your incorrect answers:")
         for question, user_choice, correct_answer in incorrect_answers:
             print(f"Question: {question}\nYour answer: {user_choice}\nCorrect answer: {correct_answer}\n")
+
+# Validate the user's input to make sure it is a valid number within the correct range
+def validate_user_input(user_input, max_valid):
+    try:
+        user_choice = int(user_input)
+        if 1 <= user_choice <= max_valid:
+            return user_choice
+        else:
+            print(f"Invalid selection. Please choose a number between 1 and {max_valid}.")
+            return None
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        return None
+
+# Print the results of the quiz and save them to a text file
+def print_and_save_results(num_correct, num_questions, incorrect_answers):
+    print(f"Your score: {num_correct}/{num_questions}")
+
+    if incorrect_answers:
+        print("\nReview your incorrect answers:")
+        for question, user_choice, correct_answer in incorrect_answers:
+            print(f"Question: {question}\nYour answer: {user_choice}\nCorrect answer: {correct_answer}\n")
+
+    with open('quiz_results.txt', 'w') as f:
+        f.write(f"Your score: {num_correct}/{num_questions}\n")
+        if incorrect_answers:
+            f.write("\nReview your incorrect answers:\n")
+            for question, user_choice, correct_answer in incorrect_answers:
+                f.write(f"Question: {question}\nYour answer: {user_choice}\nCorrect answer: {correct_answer}\n")
 
 def select_excel_file():
     files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.xlsx')]
@@ -86,6 +138,10 @@ if __name__ == "__main__":
     file_name = select_excel_file()
     if file_name:
         flashcards = load_flashcards(file_name)
-        quiz_flashcards(flashcards)
+        # Add a check to ensure flashcards is not None
+        if flashcards:
+            quiz_flashcards(flashcards)
+        else:
+            print("Failed to load flashcards from the file. Please check the file and try again.")
     else:
         print("Please add an Excel file to the current directory and try again.")
--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 import pandas as pd
 import random
@@ -14,14 +16,14 @@ def load_flashcards(file):
 def get_multiple_choices(flashcards, correct_answer):
     incorrect_flashcards = [fc['Answer'] for fc in flashcards if fc['Answer'] != correct_answer]
     choices = [correct_answer] + random.sample(incorrect_flashcards, 3)
-    choices.sort()
+    random.shuffle(choices)
     return choices
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         file = request.files['file']
-        if file.filename.endswith('.xlsx'):
+        if file and file.filename.endswith('.xlsx'):
             flashcards = load_flashcards(file)
             random.shuffle(flashcards)
             session['flashcards'] = flashcards
@@ -39,54 +41,49 @@ def quiz():
     if 'flashcards' not in session:
         return redirect(url_for('index'))
     
+    flashcards = session['flashcards']
+    current_question = session['current_question']
+    question = flashcards[current_question]['Question']
+    answer = flashcards[current_question]['Answer']
+    choices = get_multiple_choices(flashcards, answer)
+
     if request.method == 'POST':
         user_answer = request.form.get('user_answer', '').strip()
 
         if user_answer.lower() == 'stop':
             return redirect(url_for('results'))
 
-        flashcards = session['flashcards']
-        current_question = session['current_question']
-        answer = flashcards[current_question]['Answer']
-        choices = get_multiple_choices(flashcards, answer)
-
         try:
             user_choice = choices[int(user_answer) - 1]
         except (ValueError, IndexError):
-            flash("Invalid input. Skipping this question.", 'error')
+            flash("Invalid input. Please enter a number corresponding to one of the choices. Skipping this question.", 'error')
         else:
             if user_choice.strip().lower() == answer.strip().lower():
                 flash("Correct!", 'success')
                 session['num_correct'] += 1
             else:
                 flash(f"Wrong. The correct answer is: {answer}", 'error')
-                session['incorrect_answers'].append((flashcards[current_question]['Question'], user_choice, answer))
+                session['incorrect_answers'].append((question, user_choice, answer))
 
         session['current_question'] += 1
         if session['current_question'] >= len(session['flashcards']):
             return redirect(url_for('results'))
 
-    return render_template('quiz.html', get_multiple_choices=get_multiple_choices)
+    return render_template('quiz.html', question=question, choices=choices)
 
 @app.route('/results', methods=['GET', 'POST'])
 def results():
     if 'flashcards' in session:
         num_questions = len(session['flashcards'])
         num_correct = session['num_correct']
-        num_answered = session['current_question']
         incorrect_answers = session['incorrect_answers']
 
         # Calculate the percentage of correct answers
-        if num_answered != 0:
-            score_percentage = (num_correct / num_answered) * 100
-        else:
-            score_percentage = None
+        score_percentage = (num_correct / num_questions) * 100
 
-        return render_template('result.html', num_correct=num_correct, num_questions=num_questions, num_answered=num_answered, score_percentage=score_percentage, incorrect_answers=incorrect_answers)
+        return render_template('results.html', num_correct=num_correct, num_questions=num_questions, score_percentage=score_percentage, incorrect_answers=incorrect_answers)
     else:
         return redirect(url_for('index'))
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
